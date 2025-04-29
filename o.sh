58ce1f30ce35865396f1be82563a3fdffcd4f8eb
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2025-04-29 10:55:36 +0300

    Add files via upload

---
+++ o.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+gcc -std=c2x -c src/conio.c -o bin/conio.o
+gcc -std=c2x -c src/system.c -o bin/system.o
+gcc -std=c2x -c src/io.c -o bin/io.o
+gcc -std=c2x -c src/algorithm.c -o bin/algorithm.o
+gcc -std=c2x -c src/string.c -o bin/string.o
+gcc -std=c2x -c src/timer.c -o bin/timer.o
+gcc -std=c2x -c src/dt.c -o bin/dt.o
+gcc -std=c2x -c src/line.c -o bin/line.o
+gcc -std=c2x -c src/window.c -o bin/window.o
+gcc -std=c2x -c src/indicators.c -o bin/indicators.o
