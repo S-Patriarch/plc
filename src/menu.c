573780d6488df140231872c25a0464c56207eaa6
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2025-05-03 19:45:03 +0300

    Add files via upload

---
+++ src/menu.c
@@ -0,0 +1,96 @@
+/*
+ * (C) 2025, S-Patriarch
+ * This file is part of the PLC library.
+ *
+ * Patriarch Library C : menu.c
+ */
+
+#include <plc/plcdef.h>
+#include <plc/menu.h>
+#include <plc/conio.h>
+#include <string.h>
+#include <stdio.h>
+
+void setmenu(const char *attrcolor_n,
+             const char *attrcolor_s,
+             menu_style_s *mst)
+{
+        if (attrcolor_n == NULL || *attrcolor_n == '\0')
+                strncpy(mst->m_attrcolor_n, "\033[39;49m", LEN_ATTR_COLOR-1);
+        else    strncpy(mst->m_attrcolor_n, attrcolor_n, LEN_ATTR_COLOR-1);
+        mst->m_attrcolor_n[LEN_ATTR_COLOR-1] = '\0';
+
+        if (attrcolor_s == NULL || *attrcolor_s == '\0')
+                strncpy(mst->m_attrcolor_s, "\033[7m", LEN_ATTR_COLOR-1);
+        else    strncpy(mst->m_attrcolor_s, attrcolor_s, LEN_ATTR_COLOR-1);
+        mst->m_attrcolor_s[LEN_ATTR_COLOR-1] = '\0';
+}
+
+void mdraw(const menu_style_s *mst,
+           const menu_s *m,
+           size_t menu_size,
+           size_t selected)
+{
+        for (size_t i = 0; i != menu_size; ++i) {
+                if (i == selected) {
+                        resattr();
+                        setattr(mst->m_attrcolor_s);
+                        cursor_move(m[i].m_y, m[i].m_x);
+                        printf("%s", m[i].m_name);
+                } else {
+                        resattr();
+                        setattr(mst->m_attrcolor_n);
+                        cursor_move(m[i].m_y, m[i].m_x);
+                        printf("%s", m[i].m_name);
+                }
+        }
+
+        fflush(stdout);
+
+        char c;
+        for (;;) {
+                getch(&c);
+
+                if (c == ESC) {
+                        char key[3];
+
+                        getch(&c);
+                        key[0] = c;
+                        getch(&c);
+                        key[1] = c;
+
+                        // cтрелка вверх - влево
+                        if (key[0] == '[' && (key[1] == 'A' || key[1] == 'D'))
+                                if (selected > 0) {
+                                        --selected;
+                                        resattr();
+                                        setattr(mst->m_attrcolor_n);
+                                        cursor_move(m[selected+1].m_y, m[selected+1].m_x);
+                                        printf("%s", m[selected+1].m_name);
+                                        resattr();
+                                        setattr(mst->m_attrcolor_s);
+                                        cursor_move(m[selected].m_y, m[selected].m_x);
+                                        printf("%s", m[selected].m_name);
+                                        fflush(stdout);
+                                }
+                        // cтрелка вниз - вправо
+                        if (key[0] == '[' && (key[1] == 'B' || key[1] == 'C'))
+                                if (selected < menu_size-1) {
+                                        ++selected;
+                                        resattr();
+                                        setattr(mst->m_attrcolor_n);
+                                        cursor_move(m[selected-1].m_y, m[selected-1].m_x);
+                                        printf("%s", m[selected-1].m_name);
+                                        resattr();
+                                        setattr(mst->m_attrcolor_s);
+                                        cursor_move(m[selected].m_y, m[selected].m_x);
+                                        printf("%s", m[selected].m_name);
+                                        fflush(stdout);
+                                }
+                }
+                else if (c == '\n')
+                        m[selected].function();
+        }
+
+        resattr();
+}
