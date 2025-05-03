7d306958479cc08673440608ecf5946a7800bd7e
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2025-05-03 19:45:45 +0300

    Add files via upload

--- test/test.sh
+++ test/test.sh
@@ -8,4 +8,5 @@
 #gcc -std=c2x test_timer.c -lplc -o t.out
 #gcc -std=c2x test_dt.c -lplc -o t.out
 #gcc -std=c2x test_line.c -lplc -o t.out
-gcc -std=c2x test_indicators.c -lplc -o t.out
+#gcc -std=c2x test_indicators.c -lplc -o t.out
+gcc -std=2cx test_menu.c -lplc -o t.out
