2496762d5b2a43be59348390436f764851b363c6
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2025-05-30 10:14:00 +0300

    Add files via upload

--- include/tcpip.h
+++ include/tcpip.h
@@ -9,21 +9,11 @@
 #define __PLC_TCPIP_H   1
 
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <stddef.h>
-
-const int LISTENQ     = 1024; // максимальное количество клиентских соединений 
-const int MAXLINE     = 4096; // максимальная длина текстовой строки
-const int BUFFSIZE    = 8192; // размер буфера для чтения и записи
-const int MAXSOCKADDR = 128;  // максимальный размер структуры адреса сокета
-const int SERV_PORT   = 9877; // клиент-серверы TCP и UDP
+#include <stdio.h>
 
 typedef void Sigfunc(int); // для обработчиков сигналов
-
-typedef struct tcpip {
-        int   _read_cnt;
-        char *_read_ptr;
-        char  _read_buf[MAXLINE];
-} tcpip_s;
 
 /* создание сокета */
 int tcp_socket(int domain, int type, int protocol);
@@ -56,5 +46,36 @@ ssize_t tcp_readline(int fd, void *ptr, size_t maxlen);
 /* запись данных в поток */
 int tcp_write(int fd, const void *ptr, size_t nbytes);
 int tcp_writen(int fd, const void *ptr, size_t nbytes);
+
+/* порождение дочернего процесса */
+pid_t tcp_fork(void);
+
+/* обработчик сигналов */
+Sigfunc *tcp_signal(int signo, Sigfunc *func);
+
+/* установка признака конца файла в стандартном потоке ввода
+   отправка сегмента FIN */
+int tcp_shutdown(int fd, int how);
+
+/* ввод из потока */
+char *tcp_fgets(char *str, int count, FILE *stream);
+
+/* вывод в поток */
+int tcp_fputs(const char *str, FILE *stream);
+
+/* создание потока */
+int tcp_pthread_create(pthread_t *tid, 
+                       const pthread_attr_t *attr, 
+                       void *(*func)(void*), 
+                       void *arg);
+
+/* завершение потока */
+int tcp_pthread_join(pthread_t tid, void **status);
+
+/* отсоединение потока */
+int tcp_pthread_detach(pthread_t tid);
+
+/* уничтожение потока */
+int tcp_pthread_kill(pthread_t tid, int signo);
 
 #endif  /* __PLC_TCPIP_H */
