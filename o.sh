de2a0b0ce504d89f39c2ad64386d2c4193b1b65e
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2025-10-26 10:54:27 +0300

    Add files via upload

--- o.sh
+++ o.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-gcc -std=c2x -Wall -O3 -c src/conio.c -o bin/conio.o
-gcc -std=c2x -Wall -O3 -c src/system.c -o bin/system.o
-gcc -std=c2x -Wall -O3 -c src/io.c -o bin/io.o
-gcc -std=c2x -Wall -O3 -c src/algorithm.c -o bin/algorithm.o
-gcc -std=c2x -Wall -O3 -c src/string.c -o bin/string.o
-gcc -std=c2x -Wall -O3 -c src/timer.c -o bin/timer.o
-gcc -std=c2x -Wall -O3 -c src/dt.c -o bin/dt.o
-gcc -std=c2x -Wall -O3 -c src/line.c -o bin/line.o
-gcc -std=c2x -Wall -O3 -c src/window.c -o bin/window.o
-gcc -std=c2x -Wall -O3 -c src/indicators.c -o bin/indicators.o
-gcc -std=c2x -Wall -O3 -c src/menu.c -o bin/menu.o
-gcc -std=c2x -Wall -O3 -c src/logger.c -o bin/logger.o
-gcc -std=c2x -Wall -O3 -c src/tcpip.c -o bin/tcpip.o
-gcc -std=c2x -Wall -O3 -c src/stack.c -o bin/stack.o -lpthread
-gcc -std=c2x -Wall -O3 -c src/async.c -o bin/async.o -lpthread
+gcc -c src/conio.c -o bin/conio.o -Wall -O3 -std=c2x
+gcc -c src/system.c -o bin/system.o -Wall -O3 -std=c2x
+gcc -c src/io.c -o bin/io.o -Wall -O3 -std=c2x
+gcc -c src/algorithm.c -o bin/algorithm.o -Wall -O3 -std=c2x
+gcc -c src/string.c -o bin/string.o -Wall -O3 -std=c2x
+gcc -c src/timer.c -o bin/timer.o -Wall -O3 -std=c2x
+gcc -c src/dt.c -o bin/dt.o -Wall -O3 -std=c2x
+gcc -c src/line.c -o bin/line.o -Wall -O3 -std=c2x
+gcc -c src/window.c -o bin/window.o -Wall -O3 -std=c2x
+gcc -c src/indicators.c -o bin/indicators.o -Wall -O3 -std=c2x
+gcc -c src/menu.c -o bin/menu.o -Wall -O3 -std=c2x
+gcc -c src/tcpip.c -o bin/tcpip.o -Wall -O3 -std=c2x
+gcc -c src/logger.c -o bin/logger.o -Wall -O3 -lpthread
+gcc -c src/stack.c -o bin/stack.o -Wall -O3 -std=c2x -lpthread
+gcc -c src/async.c -o bin/async.o -Wall -O3 -std=c2x -lpthread
