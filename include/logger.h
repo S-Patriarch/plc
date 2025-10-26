e8eedb67dda37464979a16f3d0e4c224252a3d66
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2025-10-26 10:55:26 +0300

    Add files via upload

--- include/logger.h
+++ include/logger.h
@@ -9,18 +9,17 @@
 #define __PLC_LOGGER_H  1
 
 #include <stdio.h>
-#include <omp.h>
+#include <pthread.h>
 
 typedef struct logger {
-        FILE            *file;           /* указатель на файл логов */
-        omp_lock_t       lck_write;      /* блокировка для записи (эксклюзивный доступ) */
-        omp_nest_lock_t  lck_read;       /* блокировка для чтения (разделяемый доступ) */
+        FILE             *file;       /* указатель на файл логов */
+        pthread_mutex_t   lck_write;  /* блокировка для записи (эксклюзивный доступ) */
+        pthread_rwlock_t  lck_read;   /* блокировка для чтения (разделяемый доступ) */
 } logger_s;
 
-logger_s *p_logger_create(const char *file_name);     /* создание нового логгера */
-void      p_logger_destroy(logger_s *log);            /* уничтожение логгера и освобождение ресурсов */
-
-void  p_logger_write(logger_s *log, const char *msg); /* запись сообщений в лог (потокобезопасная) */
-char *p_logger_read(logger_s *log);                   /* чтение строки из лога (потокобезопасное) */
+logger_s *p_logger_create(const char *file_name);          /* создание нового логгера */
+void      p_logger_destroy(logger_s *log);                 /* уничтожение логгера и освобождение ресурсов */
+void      p_logger_write(logger_s *log, const char *msg);  /* запись сообщений в лог (потокобезопасная) */
+char     *p_logger_read(logger_s *log);                    /* чтение строки из лога (потокобезопасное) */
 
 #endif  /* __PLC_LOGGER_H */
