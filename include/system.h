e6f5535c4f964530deab05f7be47d7aff2dadf8f
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-03-23 11:28:46 +0300

    Add files via upload

--- include/system.h
+++ include/system.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2025, S-Patriarch
+ * (C) 2025-26, S-Patriarch
  * This file is part of the PLC library.
  *
  * Patriarch Library C : system.h
@@ -9,6 +9,8 @@
 #define __PLC_SYSTEM_H  1
 
 #include <plc/plcdef.h>
+#include <time.h>
+#include <sys/time.h>
 
 /* Определение доступности языковой локали.  */
 int     p_locale_available(const char *locale) P_NOEXCEPT;
@@ -32,5 +34,20 @@ char   *p_getlocale(void) P_NOEXCEPT;
  *      p_free_ptr((void **)&ip);
  */
 void    p_free_ptr(void **ptr) P_NOEXCEPT;
+
+/* Данная функция используется при необходимости указания абсолютного времени, 
+ * а не относительного. 
+ *
+ * Например, если потребуется ограничить время ожидания 3 минутами, мы должны
+ * сохранить в структуре timespec не 3 минуты, а текущее время + 3 минуты.
+ * Для этого можно воспользоваться функцией clock_gettime, возвращающей
+ * текущее время в виде структуры timespec. Однако эта функция поддерживается
+ * не всеми платформами. Вместо нее можно использовать функцию gettimeofday,
+ * чтобы получить текущее время в виде структуры timeval, и затем 
+ * преобразовать ее в структуру timespec.
+ *
+ * Возвращает 0 в случае успеха, -1 - в случае неудачи.
+ */
+int     p_make_timeout(struct timespec *tsp, long minutes) P_NOEXCEPT;
 
 #endif  /* __PLC_SYSTEM_H  */
