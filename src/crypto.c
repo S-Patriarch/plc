77229144aa07fa8da7af2b7a3e48109322419421
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-03-27 14:03:22 +0300

    Add files via upload

--- src/crypto.c
+++ src/crypto.c
@@ -7,7 +7,10 @@
 
 #include <plc/crypto.h>
 #include <plc/plcdef.h>
+#include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <ctype.h>
 
 unsigned int 
 p_ptr_list_hash(void **in) P_NOEXCEPT 
@@ -47,5 +50,90 @@ p_strhash64(const char *s) P_NOEXCEPT
         }
 
         return(hash);
+}
+
+int
+p_crle(const char *s, char *out) P_NOEXCEPT
+{
+        int     len = strlen(s);
+        int     i, cnt, written, out_pos;
+        char    c;
+
+        if (s == NULL || *s == '\0' || out == NULL)
+                return(-1);
+
+        out_pos = 0;
+
+        for (i = 0; i < len;) {
+                c = i[s];
+                cnt = 1;
+
+                /* подсчет количества повторяющихся символов  */
+                while ((i+cnt) < len && (i+cnt)[s] == c)
+                        ++cnt;
+
+                /* проверка на переполнение буфера 
+                 * (каждая группа: число + символ + возможно завершающий ноль)
+                 * число cnt может занимать до 10 цифр (для 32-битного int)
+                 * 10 цифр + 1 символ = 11 байт на группу, плюс место для 
+                 * завершающего нуля
+                 */
+                if ((out_pos + 12) > (len * 2 + 1))
+                        return(-1); /* недостаточно места в выходном буфере  */
+
+                /* формируем число в строку прямо в out  */
+                written = snprintf(out + out_pos, 12, "%d", cnt);
+                if (written < 0)
+                        return(-1); /* ошибка форматирования  */
+                out_pos += written;
+
+                /* добавляем символ  */                
+                out_pos++[out] = c;
+                
+                i += cnt;
+        }
+
+        /* добавляем завершающий нуль-терминатор  */
+        out_pos[out] = '\0';
+
+        return(0);
+}
+
+int
+p_drle(const char *s, char *out) P_NOEXCEPT
+{
+        int     i, j, cnt, out_pos;
+        char    c;
+
+        if (s == NULL || *s == '\0' || out == NULL)
+                return(-1);
+
+        out_pos = 0;
+
+        for (i = 0; i[s] != '\0';) {
+                cnt = 0;
+
+                /* парсим число  */
+                while (isdigit(i[s])) {
+                        cnt = cnt * 10 + (i[s] - '0');
+                        i++;
+                }
+
+                /* проверка на ошибки: нет числа или нет символа после числа  */
+                if (cnt == 0 || i[s] == '\0')
+                        return(-1);
+                
+                c = i[s];
+                i++; 
+                
+                /* добавляем символ cnt раз  */
+                for (j = 0; j < cnt; j++)
+                        out_pos++[out] = c;
+        }
+
+        /* добавляем завершающий нуль-терминатор  */
+        out_pos[out] = '\0';
+
+        return(0);
 }
 
