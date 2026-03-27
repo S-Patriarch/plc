3d7f0367d5e1d690179b85b435bbb62e876c8d13
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-03-27 14:02:57 +0300

    Add files via upload

--- include/crypto.h
+++ include/crypto.h
@@ -26,4 +26,14 @@ unsigned long long      p_strhash64(const char *s) P_NOEXCEPT;
 /* Хэширование списка указателей  */
 unsigned int            p_ptr_list_hash(void **in) P_NOEXCEPT;
 
+/* Функции p_crle и p_drle сжимают и востанавливают строки с помощью RLE
+ * (Run-Length Encoding).
+ * Пример входной строки: AAAABBBCCDAA
+ * Ожидаемый выход:       4A3B2C1D2A
+ * 
+ * Возвращают 0 при успешном завершении, -1 при ошибке.
+ */
+int                     p_crle(const char *s, char *out) P_NOEXCEPT;
+int                     p_drle(const char *s, char *out) P_NOEXCEPT;
+
 #endif  /* __PLC_CRYPTO_H  */
