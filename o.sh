2ca48d91396f44532e6ba19cde5feb6260029e1f
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-04-28 19:30:05 +0300

    Add files via upload

--- o.sh
+++ o.sh
@@ -17,3 +17,4 @@ gcc -c src/async.c -o bin/async.o -Wall -O3 -std=c2x -lpthread
 gcc -c src/file.c -o bin/file.o -Wall -O3
 gcc -c src/error.c -o bin/error.o -Wall -O3 -std=c2x
 gcc -c src/crypto.c -o bin/crypto.o -Wall -O3 -std=c2x
+gcc -c src/bitset.c -o bin/bitset.o -Wall -O3 -std=c2x
