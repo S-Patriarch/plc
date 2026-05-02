d879a312499c4897e54d5f97408ee20de2184438
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-05-02 16:24:22 +0300

    Add files via upload

--- include/io.h
+++ include/io.h
@@ -18,11 +18,11 @@ enum mess_format {
 };
 
 /* Вывод сообщения в stdout.  */
-int p_mout (const char *message, enum mess_format format, 
-            const char *attr_m, const char *attr_f) P_NOEXCEPT;
+extern int p_mout (const char *message, enum mess_format format, 
+                   const char *attr_m, const char *attr_f) P_NOEXCEPT;
 
 /* Вывод строки в stdout.  */
-int p_cout (const char *s) P_NOEXCEPT;
+extern int p_cout (const char *s) P_NOEXCEPT;
 
 /* Функция записывает строку в стандартный поток вывода stdout,
    автоматически добавляя в конце символ новой строки '\n'.
@@ -32,13 +32,13 @@ int p_cout (const char *s) P_NOEXCEPT;
    - После строки всегда добавляет символ перевода строки '\n'.
    - Использует stdout как поток вывода.
    - При успехе сбрасывает буфер stdout.  */
-int p_puts (const char *s) P_NOEXCEPT;
+extern int p_puts (const char *s) P_NOEXCEPT;
 
 /* Функция принимает ввод.
    Функция циклически считывает по одному символу. Цикл прерывается при 
    чтении либо EOF, либо символа перевода строки '\n', либо достижения 
    границы массива len. Когда ввод достигает максимального количества
    символов, функция добавляет в dst символ '\0'.  */
-void p_gets (char *dst, size_t len) P_NOEXCEPT;
+extern void p_gets (char *dst, size_t len) P_NOEXCEPT;
 
 #endif  /* __PLC_IO_H  */
