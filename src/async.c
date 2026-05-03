b0c8be0da03487ff9fd4cc58deb0633ece2e1cb8
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-05-03 16:25:12 +0300

    Add files via upload

--- src/async.c
+++ src/async.c
@@ -1,39 +1,44 @@
-#include <plc/async.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <plc/async.h>
 
-future_s *p_async_create(void *(*func)(void*), void *arg) P_NOEXCEPT 
+struct future_s *
+p_async_create(void *(*func)(void*), void *arg) P_NOEXCEPT 
 {
-        future_s *future = (future_s*)malloc(sizeof(future_s));
-        if (!future) return NULL;
+        struct future_s *future = 
+                (struct future_s*)malloc(sizeof(struct future_s));
+        if (!future) return (NULL);
 
         future->function  = func;
         future->argument  = arg;
         future->completed = false;
         future->result    = NULL;
 
-        return future;
+        return (future);
 }
 
-int p_async_launch(future_s *future) P_NOEXCEPT
+int 
+p_async_launch(struct future_s *future) P_NOEXCEPT
 {
-        return pthread_create(&future->thread, 
-                              NULL, 
-                              future->function, 
-                              future->argument);
+        return (pthread_create(&future->thread, 
+                               NULL, 
+                               future->function, 
+                               future->argument));
 }
 
-void *p_async_get(future_s *future) P_NOEXCEPT
+void *
+p_async_get(struct future_s *future) P_NOEXCEPT
 {
         if (!future->completed) {
                 pthread_join(future->thread, &future->result);
                 future->completed = true;
         }
         
-        return future->result;
+        return (future->result);
 }
 
-void p_async_free(future_s *future) P_NOEXCEPT
+void 
+p_async_free(struct future_s *future) P_NOEXCEPT
 {
         if (!future) return;
 
