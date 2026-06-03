e95669ea57a8c410d50750d3692105e417cb9cb8
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-06-03 21:35:52 +0300

    Add files via upload

--- o.sh
+++ o.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-#gcc -c src/conio.c -o bin/conio.o -Wall -O3 -std=c2x
-#gcc -c src/system.c -o bin/system.o -Wall -O3 -std=c2x
-#gcc -c src/io.c -o bin/io.o -Wall -O3 -std=c2x
-#gcc -c src/algorithm.c -o bin/algorithm.o -Wall -O3 -std=c2x
-#gcc -c src/string.c -o bin/string.o -Wall -O3 -std=c2x
-#gcc -c src/timer.c -o bin/timer.o -Wall -O3 -std=c2x
-#gcc -c src/dt.c -o bin/dt.o -Wall -O3 -std=c2x
-#gcc -c src/line.c -o bin/line.o -Wall -O3 -std=c2x
-#gcc -c src/window.c -o bin/window.o -Wall -O3 -std=c2x
-#gcc -c src/indicators.c -o bin/indicators.o -Wall -O3 -std=c2x
-#gcc -c src/menu.c -o bin/menu.o -Wall -O3 -std=c2x
-#gcc -c src/tcpip.c -o bin/tcpip.o -Wall -O3 -std=c2x
-#gcc -c src/logger.c -o bin/logger.o -Wall -O3 -lpthread
-#gcc -c src/stack.c -o bin/stack.o -Wall -O3 -std=c2x -lpthread
-#gcc -c src/async.c -o bin/async.o -Wall -O3 -std=c2x -lpthread
-#gcc -c src/file.c -o bin/file.o -Wall -O3
-#gcc -c src/error.c -o bin/error.o -Wall -O3 -std=c2x
-#gcc -c src/crypto.c -o bin/crypto.o -Wall -O3 -std=c2x
-#gcc -c src/bitset.c -o bin/bitset.o -Wall -O3 -std=c2x
-#gcc -c src/inet.c -o bin/inet.o -Wall -O3
-#gcc -c src/ringbuf.c -o bin/ringbuf.o -Wall -O3 -std=c2x
-#gcc -c src/thread.c -o bin/thread.o -Wall -O3 -std=c2x
+gcc -c src/conio.c -o bin/conio.o -Wall -O3 -std=c2x
+gcc -c src/system.c -o bin/system.o -Wall -O3 -std=c2x
+gcc -c src/io.c -o bin/io.o -Wall -O3 -std=c2x
+gcc -c src/algorithm.c -o bin/algorithm.o -Wall -O3 -std=c2x
+gcc -c src/string.c -o bin/string.o -Wall -O3 -std=c2x
+gcc -c src/timer.c -o bin/timer.o -Wall -O3 -std=c2x
+gcc -c src/dt.c -o bin/dt.o -Wall -O3 -std=c2x
+gcc -c src/line.c -o bin/line.o -Wall -O3 -std=c2x
+gcc -c src/window.c -o bin/window.o -Wall -O3 -std=c2x
+gcc -c src/indicators.c -o bin/indicators.o -Wall -O3 -std=c2x
+gcc -c src/menu.c -o bin/menu.o -Wall -O3 -std=c2x
+gcc -c src/tcpip.c -o bin/tcpip.o -Wall -O3 -std=c2x
+gcc -c src/logger.c -o bin/logger.o -Wall -O3 -lpthread
+gcc -c src/stack.c -o bin/stack.o -Wall -O3 -std=c2x -lpthread
+gcc -c src/async.c -o bin/async.o -Wall -O3 -std=c2x -lpthread
+gcc -c src/file.c -o bin/file.o -Wall -O3
+gcc -c src/error.c -o bin/error.o -Wall -O3 -std=c2x
+gcc -c src/crypto.c -o bin/crypto.o -Wall -O3 -std=c2x
+gcc -c src/bitset.c -o bin/bitset.o -Wall -O3 -std=c2x
+gcc -c src/inet.c -o bin/inet.o -Wall -O3
+gcc -c src/ringbuf.c -o bin/ringbuf.o -Wall -O3 -std=c2x
+gcc -c src/thread.c -o bin/thread.o -Wall -O3 -std=c2x
 gcc -c src/sds.c -o bin/sds.o -Wall -O3 -std=c2x
+gcc -c src/hashmap.c -o bin/hashmap.o -Wall
