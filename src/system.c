a30b1e82589ee92642502970a3a1333c7937c9e2
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-06-07 10:07:01 +0300

    Add files via upload

--- src/system.c
+++ src/system.c
@@ -117,3 +117,63 @@ p_signal(int signo, p_sigfunc *func) P_NOEXCEPT
         return (oact.sa_handler);
 }
 
+int
+p_daemonize(const char *cmd) P_NOEXCEPT 
+{
+        int                     i, fd0, fd1, fd2;
+        pid_t                   pid;
+        struct rlimit           rl;
+        struct sigaction        sa;
+
+        /* сбросить маску режима создания файла  */
+        umask(0);
+
+        /* получить максимально возможный номер дескриптора файла  */
+        if (getrlimit(RLIMIT_NOFILE, &rl) < 0)
+                return(-1);
+
+        /* стать лидером нового сеанса, чтобы утратить управляющий терминал  */
+        if ((pid = fork()) < 0)
+                return(-1);
+        else if (pid != 0) /* родительский процесс  */
+                exit(0);
+        setsid();
+
+        /* обеспечить невозможность обретения управляющего терминала в будущем  */
+        sa.sa_handler = SIG_IGN;
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = 0;
+        if (sigaction(SIGHUP,&sa, NULL) < 0)
+                return(-1);
+        if ((pid = fork()) < 0)
+                return(-1);
+        else if (pid != 0 ) /* родительский процесс  */
+                exit(0);
+
+        /* назначить корневой каталог текущим рабочим каталогом,
+           чтобы впоследствии можно было отмонтировать файловую систему  */
+        if (chdir("/") < 0)
+                return(-1);
+
+        /* закрыть все открытые файловые дескрипторы  */
+        if (rl.rlim_max == RLIM_INFINITY)
+                rl.rlim_max = 1024;
+        for (i = 0; i < rl.rlim_max; i++)
+                close(i);
+
+        /* присоединить файловые дескрипторы 0, 1 и 2 к /dev/null  */
+        fd0 = open("/dev/null", O_RDWR);
+        fd1 = dup(0);
+        fd2 = dup(0);
+
+        /* инициализировать файл журнала  */
+        openlog(cmd, LOG_CONS, LOG_DAEMON);
+        if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
+                syslog(LOG_ERR, "ошибочные файловые дескрипторы %d %d %d",
+                       fd0, fd1, fd2);
+                return(-1);
+        }
+
+        return(0);
+}
+
