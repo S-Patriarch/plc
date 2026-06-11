4ec8cca3d67c9faed5a73dc1703212e853fe8fde
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-06-11 21:36:42 +0300

    Add files via upload

--- include/file.h
+++ include/file.h
@@ -12,6 +12,7 @@
 #include <fcntl.h>
 #include <sys/syscall.h>
 #include <plc/plcdef.h>
+#include <plc/error.h>
 
 P_BEGIN_DECLS
 
@@ -33,6 +34,39 @@ extern int p_file_setfl (int fd, int flags) P_NOEXCEPT;
 
 extern int p_file_copy (const char *f_src, const char *f_dst) P_NOEXCEPT;
 extern int p_file_move (const char *f_src, const char *f_dst) P_NOEXCEPT;
+
+/* Функция проверки возможности наложения блокировки на участок файла.
+   Если уже существует блокировка, которая может заблокировать выполнение
+   запроса с заданными параметрами, эта функция возвращает идентификатор 
+   процесса, владеющего блокировкой.  Иначе возвращается 0 (ложь).  Эту
+   функцию желательно вызывать из следующих двух макросов.  */
+extern pid_t p_lock_test (int fd, int type, off_t offset, int whence, 
+                          off_t len) P_NOEXCEPT;
+
+#define P_IS_READ_LOCKABLE(fd, offset, whence, len) \
+                (p_lock_test((fd), F_RDLCK, (offset), (whence), (len)) == 0)
+#define P_IS_WRITE_LOCKABLE(fd, offset, whence, len) \
+                (p_lock_test((fd), F_WRLCK, (offset), (whence), (len)) == 0)
+
+/* Функция наложения и снятия блокировки на участок файла.  
+   Эта функция избавляет от необходимости всякий раз размещать и заполнять
+   структуру flock.  Она выполняет все необходимые действия.  Поскольку в
+   большинстве случаев эта функция будет вызываться для наложения или
+   снятия блокировки (команда F_GETLK используется редко), желательно ее
+   вызывать из следующих пяти макросов.  */
+extern int p_lock_reg (int fd, int cmd, int type, off_t offset, int whence, 
+                       off_t len) P_NOEXCEPT;
+
+#define P_READ_LOCK(fd, offset, whence, len) \
+                p_lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len))
+#define P_READW_LOCK(fd, offset, whence, len) \
+                p_lock_reg((fd), F_SETLKW, F_RDLCK, (offset), (whence), (len))
+#define P_WRITE_LOCK(fd, offset, whence, len) \
+                p_lock_reg((fd), F_SETLK, F_WRLCK, (offset), (whence), (len))
+#define P_WRITEW_LOCK(fd, offset, whence, len) \
+                p_lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
+#define P_UN_LOCK(fd, offset, whence, len) \
+                p_lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len))
 
 P_END_DECLS
 
