256763366be16fec707495868852599e9a2343f8
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2026-08-02 21:26:34 +0300

    Add files via upload

--- src/algorithm.c
+++ src/algorithm.c
@@ -331,3 +331,13 @@ p_witness(unsigned long long int a, unsigned long long int n) P_NOEXCEPT
         return(1);                 /* a - свидетель составности n  */
 }
 
+static const int table[32] = {  0,  1, 28, 2, 29, 14, 24,  3, 30, 22, 20, 15, 
+                               25, 17,  4, 8, 31, 27, 13, 23, 21, 19, 16,  7, 
+                               26, 12, 18, 6, 11,  5, 10,  9 };
+
+int 
+p_lowest_setbit(uint32_t v) P_NOEXCEPT 
+{
+        return(table[((v & -v) * 0x077CB531U) >> 27]);
+}
+
